c931496c66247947c5916809348f0971f5843090
strongZERO196 <tennnpp1131@gmail.com>
2024-02-23 10:30:44 +0900

    ToDoList_初版作成

--- vue_project/src/main.js
+++ vue_project/src/main.js
@@ -1,46 +1,11 @@
-// Vueライブラリをインポート
-import Vue from 'vue';
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-// ルートコンポーネントであるApp.vueをインポート
-import App from './App.vue';
-import AppSecond from './AppSecondComponent.vue';
-import AppThird from './AppThirdComponent.vue';
-import AppFour from './AppFourComponent.vue';
-// 新しいVueインスタンスを作成
-new Vue({
-  // IDが'app'のDOM要素にVueインスタンスをマウントする
-  el: '#app',
-  
-  // Appコンポーネントを描画する
-  // hはcreateElement関数のエイリアスとして使用される
-  render: h => h(App)
-})
-
-// 新しいVueインスタンスを作成
-new Vue({
-  // IDが'app'のDOM要素にVueインスタンスをマウントする
-  el: '#app2',
-  
-  // Appコンポーネントを描画する
-  // hはcreateElement関数のエイリアスとして使用される
-  render: h => h(AppSecond)
-})
+Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
-  el: '#app3',
-  render: h => h(AppThird, {
-    props: {
-      initialMessage: 'aaaa'
-    }
-  })
-});
-
-
-new Vue({
-  el: '#app4',
-  render: h => h(AppFour, {
-    props: {
-      initialMessage: 'bbbb'
-    }
-  })
-});
+  render: h => h(App),
+}).$mount('#app')
